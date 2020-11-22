a9c2b9910fad45e8d3cc81b2d48813f7ea3f13d9
harizikhalil <khalilharizi3@gmail.com>
2020-11-22 23:18:42 +0100

    finished react hooks checkpoint

---
+++ src/components/ListsMovies.js
@@ -0,0 +1,26 @@
+
+export const ListsMovies =[{
+    id:Math.random(),
+    title:"Avengers",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:"https://images-na.ssl-images-amazon.com/images/I/719SFBdxRtL._AC_SL1000_.jpg",
+    rate:4
+
+},
+{
+    id:Math.random(),
+    title:"Fast and Furious 8",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:"https://images-na.ssl-images-amazon.com/images/I/51wUNfZfAuL._AC_.jpg",
+    rate:5
+
+},
+{
+    id:Math.random(),
+    title:"The lion King",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:"https://images-na.ssl-images-amazon.com/images/I/715DGa4kWVL._AC_SL1000_.jpg",
+    rate:4
+
+}
+]
